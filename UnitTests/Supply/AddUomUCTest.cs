b214a95442a9268dc8b54d9756c038e5ecebb33c
emaeroles <emanueleroles@gmail.com>
2025-02-28 15:28:47 -0300

    test: :test_tube: add Units of Mesure Use Cases Tests

---
+++ UnitTests/Supply/AddUomUCTest.cs
@@ -0,0 +1,64 @@
+﻿using Application.DTOs.Supply;
+using Application.Entities;
+using Application.Enums;
+using Application.Interfaces._01_Common;
+using Application.UseCases.Supply;
+using AutoMapper;
+using Moq;
+
+namespace UnitTests.Supply
+{
+    [TestClass]
+    public class AddUomUCTest
+    {
+        [TestMethod]
+        public void AddUom_ShouldReturnCreatedWithData()
+        {
+            // Arrange
+            Mock<IAddRepo<UoMEntity>> addUomRepo = new Mock<IAddRepo<UoMEntity>>();
+            Mock<IMapper> mapper = new Mock<IMapper>();
+
+            AddUomInput addUomInput = new AddUomInput();
+            UoMEntity uomEntity = new UoMEntity();
+
+            mapper.Setup(m => m.Map<UoMEntity>(addUomInput)).Returns(uomEntity);
+            addUomRepo.Setup(r => r.AddAsync(uomEntity)).ReturnsAsync(1);
+
+            AddUomUseCase addUomUseCase = new AddUomUseCase(addUomRepo.Object, mapper.Object);
+
+            ResultState resultState = ResultState.Created;
+
+            // Act
+            var result = addUomUseCase.Execute(addUomInput);
+
+            // Assert
+            Assert.AreEqual(result.Result.ResultState, resultState);
+            Assert.IsNotNull(result.Result.Data);
+        }
+
+        [TestMethod]
+        public void AddUom_ShouldReturnCreatedWithNullData()
+        {
+            // Arrange
+            Mock<IAddRepo<UoMEntity>> addUomRepo = new Mock<IAddRepo<UoMEntity>>();
+            Mock<IMapper> mapper = new Mock<IMapper>();
+
+            UoMEntity uomEntity = new UoMEntity();
+            AddUomInput addUomInput = new AddUomInput();
+
+            mapper.Setup(m => m.Map<UoMEntity>(addUomInput)).Returns(uomEntity);
+            addUomRepo.Setup(r => r.AddAsync(uomEntity)).ReturnsAsync(0);
+
+            AddUomUseCase addUomUseCase = new AddUomUseCase(addUomRepo.Object, mapper.Object);
+
+            ResultState resultState = ResultState.NotFound;
+
+            // Act
+            var result = addUomUseCase.Execute(addUomInput);
+
+            // Assert
+            Assert.AreEqual(result.Result.ResultState, resultState);
+            Assert.IsNull(result.Result.Data);
+        }
+    }
+}
---
+++ UnitTests/Supply/UpdateUomUCTest.cs
@@ -0,0 +1,64 @@
+﻿using Application.DTOs.Supply;
+using Application.Entities;
+using Application.Enums;
+using Application.Interfaces._01_Common;
+using Application.UseCases.Supply;
+using AutoMapper;
+using Moq;
+
+namespace UnitTests.Supply
+{
+    [TestClass]
+    public class UpdateUomUCTest
+    {
+        [TestMethod]
+        public void UpdateUom_ShouldReturnSuccsessWithData()
+        {
+            // Arrange
+            Mock<IUpdateRepo<UoMEntity>> updateRepo = new Mock<IUpdateRepo<UoMEntity>>();
+            Mock<IMapper> mapper = new Mock<IMapper>();
+
+            UoMEntity uomEntity = new UoMEntity();
+            UpdateUomInput updateUomInput = new UpdateUomInput();
+
+            mapper.Setup(m => m.Map<UoMEntity>(updateUomInput)).Returns(uomEntity);
+            updateRepo.Setup(r => r.UpdateAsync(uomEntity)).ReturnsAsync(true);
+
+            UpdateUomUseCase updateUomUseCase = new UpdateUomUseCase(updateRepo.Object, mapper.Object);
+
+            ResultState resultState = ResultState.Success;
+
+            // Act
+            var result = updateUomUseCase.Execute(updateUomInput);
+
+            // Assert
+            Assert.AreEqual(result.Result.ResultState, resultState);
+            Assert.IsNull(result.Result.Data);
+        }
+
+        [TestMethod]
+        public void UpdateUom_ShouldReturnNotFoundWithNullData()
+        {
+            // Arrange
+            Mock<IUpdateRepo<UoMEntity>> updateUomRepo = new Mock<IUpdateRepo<UoMEntity>>();
+            Mock<IMapper> mapper = new Mock<IMapper>();
+
+            UoMEntity uomEntity = new UoMEntity();
+            UpdateUomInput updateUomInput = new UpdateUomInput();
+
+            mapper.Setup(m => m.Map<UoMEntity>(updateUomInput)).Returns(uomEntity);
+            updateUomRepo.Setup(r => r.UpdateAsync(uomEntity)).ReturnsAsync(false);
+
+            UpdateUomUseCase updateUomUseCase = new UpdateUomUseCase(updateUomRepo.Object, mapper.Object);
+
+            ResultState resultState = ResultState.NotFound;
+
+            // Act
+            var result = updateUomUseCase.Execute(updateUomInput);
+
+            // Assert
+            Assert.AreEqual(result.Result.ResultState, resultState);
+            Assert.IsNull(result.Result.Data);
+        }
+    }
+}
