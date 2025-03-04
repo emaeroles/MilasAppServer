6966000acbe3eb678d0c8f5b30b632dc17a9c188
emaeroles <emanueleroles@gmail.com>
2025-03-04 16:16:52 -0300

    feat: :sparkles: add dependenci inyection Gets By Id

--- Data/DataDependencyInyection.cs
+++ Data/DataDependencyInyection.cs
@@ -27,6 +27,7 @@ namespace Data
 
         private static IServiceCollection AddUserRepos(this IServiceCollection services)
         {
+            services.AddScoped<IGetByIdRepo<UserEntity>, GetByIdUserRepo>();
             services.AddScoped<IGetAllByActiveRepo<UserEntity>, GetAllUsersRepo>();
             services.AddScoped<IAddRepo<UserEntity>, AddUserRepo>();
             services.AddScoped<IUpdateRepo<UserEntity>, UpdateUserRepo>();
@@ -39,6 +40,7 @@ namespace Data
 
         private static IServiceCollection AddKioscoRepos(this IServiceCollection services)
         {
+            services.AddScoped<IGetByIdRepo<KioscoEntity>, GetByIdKioscoRepo>();
             services.AddScoped<IGetAllByActiveRepo<KioscoEntity>, GetAllKioscosRepo>();
             services.AddScoped<IAddRepo<KioscoEntity>, AddKioscoRepo>();
             services.AddScoped<IUpdateRepo<KioscoEntity>, UpdateKioscoRepo>();
@@ -51,11 +53,13 @@ namespace Data
 
         private static IServiceCollection AddSupplyRepos(this IServiceCollection services)
         {
+            services.AddScoped<IGetByIdRepo<SupplyEntity>, GetByIdSupplyRepo>();
             services.AddScoped<IGetAllByActiveRepo<SupplyEntity>, GetAllSuppliesRepo>();
             services.AddScoped<IAddRepo<SupplyEntity>, AddSupplyRepo>();
             services.AddScoped<IUpdateRepo<SupplyEntity>, UpdateSupplyRepo>();
             services.AddScoped<IToggleActiveRepo<SupplyEntity>, ToggleActiveSupplyRepo>();
 
+            services.AddScoped<IGetByIdRepo<UoMEntity>, GetByIdUomRepo>();
             services.AddScoped<IGetAllByActiveRepo<UoMEntity>, GetAllUomRepo>();
             services.AddScoped<IAddRepo<UoMEntity>, AddUomRepo>();
             services.AddScoped<IUpdateRepo<UoMEntity>, UpdateUomRepo>();
@@ -66,6 +70,7 @@ namespace Data
 
         private static IServiceCollection AddProductRepos(this IServiceCollection services)
         {
+            services.AddScoped<IGetByIdRepo<ProductEntity>, GetByIdProductRepo>();
             services.AddScoped<IGetAllByActiveRepo<ProductEntity>, GetAllProductsRepo>();
             services.AddScoped<IAddRepo<ProductEntity>, AddProductRepo>();
             services.AddScoped<IUpdateRepo<ProductEntity>, UpdateProductRepo>();
