3b068257977f863c92f2651eb22d334cec9179b4
emaeroles <emanueleroles@gmail.com>
2025-03-04 14:11:07 -0300

    refactor: :recycle: from app result, ResultState and ResultFactory

--- Application/Enums/ResultState.cs
+++ Application/Enums/ResultState.cs
@@ -2,13 +2,21 @@
 {
     public enum ResultState
     {
-        Success = 0,
-        Created = 1,
-        NotCreated = 2,
-        NotFound = 3,
-        Conflict = 4,
-        BadRequest = 5,
-        Authorized = 6,
-        Unauthorized = 7,
+        Success,
+
+        Data,
+        NotFound,
+        Conflict,
+        BadRequest,
+
+        Created,
+        NotCreated,
+        Updated,
+        NotUpdated,
+        Deleted,
+        NotDeleted,
+
+        Authorized,
+        Unauthorized,
     }
 }
--- Application/Factories/ResultFactory.cs
+++ Application/Factories/ResultFactory.cs
@@ -5,9 +5,30 @@ namespace Application.Factories
 {
     internal static class ResultFactory
     {
+        // TODO: Quitar este metodo
         public static AppResult CreateSuccess(string message, object? data)
         {
             return new AppResult(message, data, ResultState.Success);
+        }
+
+        public static AppResult CreateData(string message, object data)
+        {
+            return new AppResult(message, data, ResultState.Data);
+        }
+
+        public static AppResult CreateNotFound(string message)
+        {
+            return new AppResult(message, null, ResultState.NotFound);
+        }
+
+        public static AppResult CreateConflict(string message)
+        {
+            return new AppResult(message, null, ResultState.Conflict);
+        }
+
+        public static AppResult CreateBadRequest(string message)
+        {
+            return new AppResult(message, null, ResultState.BadRequest);
         }
 
         public static AppResult CreateCreated(string message)
@@ -20,14 +41,24 @@ namespace Application.Factories
             return new AppResult(message, null, ResultState.NotCreated);
         }
 
-        public static AppResult CreateNotFound(string message)
+        public static AppResult CreateUpdated(string message)
         {
-            return new AppResult(message, null, ResultState.NotFound);
+            return new AppResult(message, null, ResultState.Updated);
         }
 
-        public static AppResult CreateConflict(string message)
+        public static AppResult CreateNotUpdated(string message)
         {
-            return new AppResult(message, null, ResultState.Conflict);
+            return new AppResult(message, null, ResultState.NotUpdated);
+        }
+
+        public static AppResult CreateDeleted(string message)
+        {
+            return new AppResult(message, null, ResultState.Deleted);
+        }
+
+        public static AppResult CreateNotDeleted(string message)
+        {
+            return new AppResult(message, null, ResultState.NotDeleted);
         }
 
         public static AppResult CreateAuthorized(string message)
