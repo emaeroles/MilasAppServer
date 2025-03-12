660c5fd80ce137c0fef06781a9c0139f523fd8e4
emaeroles <emanueleroles@gmail.com>
2025-03-12 08:51:38 -0300

    refactor: :recycle: modify get method in visit controller

--- API/Controllers/VisitController.cs
+++ API/Controllers/VisitController.cs
@@ -10,23 +10,29 @@ namespace API.Controllers
     [ApiController]
     public class VisitController : ControllerBase
     {
-        [HttpGet("{dateString}/{quantity}/get-starting-date")]
-        public async Task<IActionResult> GetStartingDate(
-            string dateString,
-            int quantity,
+        [HttpGet("get")]
+        public async Task<IActionResult> GetLastTenVisits(
+            //string dateString,
+            //int quantity,
             VisitUseCases visitUseCases)
         {
-            var validator = new InlineValidator<string>();
-            validator.RuleFor(x => x)
-                .Matches(@"^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$")
-                .WithMessage("The date must be a string in the format yyyy-MM-dd.");
-            
-            var validResult = await validator.ValidateAsync(dateString);
-            if (!validResult.IsValid)
-                throw new ValidationException(validResult.Errors);
+            //var validator = new InlineValidator<string>();
+            //validator.RuleFor(x => x)
+            //    .Matches(@"^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$")
+            //    .WithMessage("The date must be a string in the format yyyy-MM-dd.");
 
-            DateOnly date = DateOnly.Parse(dateString);
-            var appResult = await visitUseCases.GetVisitsUseCase.Execute(date, quantity);
+            //var validResult = await validator.ValidateAsync(dateString);
+            //if (!validResult.IsValid)
+            //    throw new ValidationException(validResult.Errors);
+
+            //DateOnly date = DateOnly.Parse(dateString);
+
+            DateTime dateTime = DateTime.Now;
+            DateOnly dateOnly = new DateOnly(dateTime.Year, dateTime.Month, dateTime.Day);
+
+            int quantity = 10;
+
+            var appResult = await visitUseCases.GetVisitsUseCase.Execute(dateOnly, quantity);
             return ResponseConverter.Execute(appResult);
         }
 
